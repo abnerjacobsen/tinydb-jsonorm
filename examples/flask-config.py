6675eb501d48ee3154ecc1a9b4c7af1f36e963c3
Abner G Jacobsen <abner@apoana.com.br>
2016-05-10 21:46:07 -0300

    Flask config load from tinydb-json working.

--- examples/flask-config.py
+++ examples/flask-config.py
@@ -9,12 +9,16 @@ import os
 import time
 from datetime import datetime
 from pprint import pprint
+import json
 
 from tinydb_jsonorm import Database
 from tinydb_jsonorm import TinyJsonModel
 from tinydb_jsonorm import fields
 
 from jsonmodels import models, validators
+
+from flask import Flask
+from flask_debug import Debug
 
 # Hard coded jsonmodels datastores
 class CacheType(models.Base):
@@ -61,7 +65,8 @@ def main():
         SQLALCHEMY_DATABASE_URI = fields.StringField(required=True, validators=[validators.Length(1, 256)])
         WTF_CSRF_ENABLED = fields.BoolField(required=True)
         TESTING = fields.BoolField(required=True)
-        AUTH_ENGINES = fields.ListField([AuthType], validators=[validators.Length(1, 8)])
+        AUTH_ENGINES = fields.ListField([AuthType])
+        FLASK_DEBUG_DISABLE_STRICT = fields.BoolField(required=True)
         _last_update = fields.DateTimeField(required=True)
         _created_at = fields.DateTimeField(required=True)
 
@@ -110,6 +115,7 @@ def main():
             AuthType(provider='Local'), 
             AuthType(provider='Daspanel')
         ],
+        FLASK_DEBUG_DISABLE_STRICT = True,
     )
     # ListField can have valued appended
     devcfg.AUTH_ENGINES.append(AuthType(provider='Getuuid'))
@@ -120,9 +126,75 @@ def main():
     # Insert/save the new record
     devcfgid = devcfg.insert()
     devcfgcuid = devcfg._cuid
-    print("\nDevcfg: ", devcfg.id, devcfg._cuid, devcfg.ENV, devcfg.AUTH_ENGINES)
+    #print("\nDevcfg: ", devcfg.id, devcfg._cuid, devcfg.ENV, devcfg.AUTH_ENGINES)
 
 
+    # Create new record for staging config
+    stgcfg = ConfigModel()
+    stgcfg.ENV = 'staging'
+    stgcfg.SECRET_KEY = os.urandom(25).encode('hex')
+    stgcfg.APP_DIR = os.path.abspath(os.path.dirname(__file__))
+    stgcfg.PROJECT_ROOT = os.path.abspath(os.path.join(stgcfg.APP_DIR, os.pardir))
+    stgcfg.BCRYPT_LOG_ROUNDS = 100
+    stgcfg.DEBUG = True
+    stgcfg.ASSETS_DEBUG = True
+    stgcfg.DEBUG_TB_ENABLED = True
+    stgcfg.DEBUG_TB_INTERCEPT_REDIRECTS = False
+    stgcfg.CACHE_TYPE = CacheType(engine='Memcached', host='localhost', port=11211)
+    stgcfg.SQLALCHEMY_TRACK_MODIFICATIONS = False
+    stgcfg.APP_NAME = 'Daspanel - Panel GUI'
+    stgcfg.APP_TMPL = 'sb-admin'
+    stgcfg.DB_NAME = 'dev.db'
+    stgcfg.DB_PATH = os.path.join('/opt/daspanel/data/db', stgcfg.DB_NAME)
+    stgcfg.SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(stgcfg.DB_PATH)
+    stgcfg.WTF_CSRF_ENABLED = False
+    stgcfg.TESTING = False
+    stgcfg.AUTH_ENGINES = [AuthType(provider='Getuuid'), AuthType(provider='Local')]
+    stgcfg.FLASK_DEBUG_DISABLE_STRICT = True
+    
+    stgcfg.validate()
+    stgcfgid = stgcfg.insert()
+    stgcfgcuid = stgcfg._cuid
+    #print("Stgcfg: ", stgcfg.id, stgcfg._cuid, stgcfg.ENV, stgcfg.AUTH_ENGINES)
+
+    # Create new record for production config
+    prodcfg = ConfigModel()
+    prodcfg.ENV = 'prod'
+    prodcfg.SECRET_KEY = os.urandom(25).encode('hex')
+    prodcfg.APP_DIR = os.path.abspath(os.path.dirname(__file__))
+    prodcfg.PROJECT_ROOT = os.path.abspath(os.path.join(prodcfg.APP_DIR, os.pardir))
+    prodcfg.BCRYPT_LOG_ROUNDS = 100
+    prodcfg.DEBUG = True
+    prodcfg.ASSETS_DEBUG = True
+    prodcfg.DEBUG_TB_ENABLED = True
+    prodcfg.DEBUG_TB_INTERCEPT_REDIRECTS = False
+    prodcfg.CACHE_TYPE = CacheType(engine='Memcached', host='localhost', port=11211)
+    prodcfg.SQLALCHEMY_TRACK_MODIFICATIONS = False
+    prodcfg.APP_NAME = 'Daspanel - Panel GUI'
+    prodcfg.APP_TMPL = 'sb-admin'
+    prodcfg.DB_NAME = 'dev.db'
+    prodcfg.DB_PATH = os.path.join('/opt/daspanel/data/db', prodcfg.DB_NAME)
+    prodcfg.SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(prodcfg.DB_PATH)
+    prodcfg.WTF_CSRF_ENABLED = False
+    prodcfg.TESTING = False
+    prodcfg.AUTH_ENGINES = [AuthType(provider='Getuuid'), AuthType(provider='Daspanel')]
+    prodcfg.FLASK_DEBUG_DISABLE_STRICT = True
+    
+    prodcfg.validate()
+    prodcfgid = prodcfg.insert()
+    prodcfgcuid = prodcfg._cuid
+    #print("Prodcfg: ", prodcfg.id, prodcfg._cuid, prodcfg.ENV, prodcfg.AUTH_ENGINES, "\n")
+
+    # Get Config record
+    myconfig = ConfigModel.get(dbj.where("ENV") == "prod").to_struct()
+    app = Flask(__name__)
+    app.config.update(myconfig)
+
+    Debug(app)
+    app.run(debug=True, port=8000, host='0.0.0.0')
+
+
+    
 if __name__ == "__main__":
     main()
 
