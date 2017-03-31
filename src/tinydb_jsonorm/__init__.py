704d3f887cc8963769ffbb116eb7e6909deeaecd
Abner G Jacobsen <abner@apoana.com.br>
2017-03-31 16:10:56 -0300

    Bump version: 0.2.0 → 0.2.1

--- src/tinydb_jsonorm/__init__.py
+++ src/tinydb_jsonorm/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .db import Database  # noqa
 from .models import TinyJsonModel  # noqa
